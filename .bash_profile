93c8a5ce3ece12faec2aed34c131eaf849d3d8f1
jeff <leterip@gmail.com>
2011-04-07 11:45:34 -0400

    basic commit to get env up. (hope it works when i reinstall sometime)

---
+++ .bash_profile
@@ -0,0 +1,7 @@
+alias gw='git add -u && git commit -m'
+alias ls='ls -FlG'
+alias gs='git status'
+alias gl='git log --format=format:"%H %cn %s"'
+export EDITOR='subl -w'
+export WORKON_HOME=$HOME/Code/envs
+source /usr/local/bin/virtualenvwrapper.sh
---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Git config
+git config --global user.name jeff
+git config --global user.email leterip@gmail.com
+
+#Sublime text link
+ln -s "/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
+#Bash profile
+ln -s .bash_profile ~/.bash_profile
+source ~/.bash_profile
+
+#Coding Environment
+mkdir -p ~/Code/envs
+
+#Virtualenv wrapper
+sudo easy_install pip
+pip install virtualenv virtualenvwrapper
+
+#Hooks
+ln -s postactivate ~/Code/envs/
+ln -s postmkvirtualenv ~/Code/envs/
